4f38383f2e84fb8c3232697f2c3571921532ebba
Matt Holland <mtholland10@gmail.com>
2016-12-17 16:32:06 -0600

    Initial commit

---
+++ src/js/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Title from './Header/Title';
+
+export default class Header extends React.Component {
+    render() {
+        console.log(this.props);
+        return (
+            <header>
+                <Title title={this.props.title}/>
+            </header>
+        );
+    }
+}
