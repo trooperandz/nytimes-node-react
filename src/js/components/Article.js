c054051b7bc186c12fb7ab1c7419bfe9cc9a4a5d
Matt Holland <mtholland10@gmail.com>
2016-12-25 01:06:22 -0600

    Add moment plugin and formatted date to Latest News blurps

--- src/js/components/Article.js
+++ src/js/components/Article.js
@@ -1,14 +1,19 @@
 import React from 'react';
 
+import moment from 'moment';
+
 export default class Article extends React.Component {
     render() {
-        const { title, abstract, url } = this.props;
+        const { title, published_date, abstract, url } = this.props;
+
+        let date = moment(new Date(published_date)).format('MM/DD/YYYY');
 
         return (
             <div class="col-md-4">
+                <p class="text-info article-date">{date}</p>
                 <h4>{title}</h4>
                 <p>{abstract}</p>
-                <a class="btn btn-default" href={url}>More info...</a>
+                <a class="btn btn-default" href={url} target="_blank">More info...</a>
             </div>
         );
     }
